549004112ed3e37bc9b29edfb104b9c71db9cb4f
irina-kavun <kavunirina@gmail.com>
2019-09-06 15:10:25 +0300

    slider

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.scss';
-import { Header } from './commons';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {Header} from './commons';
+import {HomePageSlider} from "./components";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-        <Router>
-            <Header/>
-        </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Header/>
+            </Router>
+            <HomePageSlider/>
+        </div>
+    );
 }
 
 export default App;
