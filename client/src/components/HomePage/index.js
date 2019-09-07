b7bdb012f18b9964e881d4e3acec12f47c6c62c9
kirsenkoDanIT <a.kirs@ukr.net>
2019-09-07 03:21:26 +0300

    edited

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import { HomePageSlider } from '../Slider';
-import {ProductCard} from '../ProductCard'
 import './style.scss';
 
 export const HomePage = props => {
   return (
     <div>
       <HomePageSlider />
-      <div className="home-page container">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-      </div>
+      <div className="home-page container"></div>
     </div>
   );
 };
