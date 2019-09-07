0dbd2013100522a597c64d47afa0ac09121e54d2
kirsenkoDanIT <50655852+kirsenkoDanIT@users.noreply.github.com>
2019-09-07 17:10:46 +0300

    Container (#21)
    * update header and container
    
    * update header and container

---
+++ client/src/commons/Extras/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './index.scss';
+import { Search, Cart, Auth } from '../';
+
+export const Extras = () => {
+  return (
+    <div className="extras">
+      <Search />
+      <Cart />
+      <Auth />
+    </div>
+  );
+};
