1b1cb2f23b4b02c9590255a407ae64c7d88a27f3
Ivan Kravchenko <aivan.kr@gmail.com>
2019-09-08 12:55:13 +0300

    login styles

--- client/src/commons/Login/index.js
+++ client/src/commons/Login/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
 
 import './index.scss';
 
 export const Login = (props) => {
   const el = <div className="outer" onClick={(e) => { if (e.target.className === 'outer') props.openModal(false) }}>
-    <div className="inner">
+    <div className="modal">
+      <span className="modal__close" onClick={() => props.openModal(false) }>&times;</span>
       <h2>Enter your account</h2>
-      <label htmlFor="login">Email:</label>
-      <input type="text" name="login" />
-      <label htmlFor="password">Password:</label>
-      <input type="password" name="password" />
-      <button>Login</button>
+      <div className="modal__field">
+        <label htmlFor="login">Login:</label>
+        <input type="text" name="login" className="modal__input" />
+      </div>
+      <div className="modal__field">
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" className="modal__input" />
+      </div>
+      <div className="modal__field">
+        <button className="modal__submit">Login</button>
+      </div>
       <span>or</span>
-      <button>Sign up</button>
+      <Link to="/sign_up" onClick={() => props.openModal(false) }>Sign up</Link>
     </div>
   </div>
 
