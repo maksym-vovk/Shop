484c3d24638210b8c0bf473483adfff0625ebfad
kirsenkoDanIT <a.kirs@ukr.net>
2019-09-08 23:44:05 +0300

    main

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,15 @@ import './App.scss';
 import { Header, Footer } from './commons';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import store from './store';
-import { HomePage, IMac, MacBook, IPad, IPhone, Watch, RegisterForm } from './components';
+import {
+  HomePage,
+  IMac,
+  MacBook,
+  IPad,
+  IPhone,
+  Watch,
+  RegisterForm
+} from './components';
 
 function App() {
   return (
@@ -13,15 +21,17 @@ function App() {
       <Router>
         <div className="App">
           <Header />
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/imac" component={IMac} />
-            <Route path="/macbook" component={MacBook} />
-            <Route path="/ipad" component={IPad} />
-            <Route path="/iphone" component={IPhone} />
-            <Route path="/watch" component={Watch} />
-            <Route path="/sign_up" component={RegisterForm} />
-          </Switch>
+          <main className="main-content">
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/imac" component={IMac} />
+              <Route path="/macbook" component={MacBook} />
+              <Route path="/ipad" component={IPad} />
+              <Route path="/iphone" component={IPhone} />
+              <Route path="/watch" component={Watch} />
+              <Route path="/sign_up" component={RegisterForm} />
+            </Switch>
+          </main>
           <Footer />
         </div>
       </Router>
