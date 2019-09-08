5e32bec3914c5ebe77e0057d060791c41b8ab49b
Ivan Kravchenko <aivan.kr@gmail.com>
2019-09-08 17:50:51 +0300

    POST user to db

--- client/src/components/ReduxForm/showResults.js
+++ client/src/components/ReduxForm/showResults.js
@@ -1,7 +1,17 @@
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+import axios from 'axios';
 
 export default (async function showResults(values) {
-  await sleep(500); // simulate server latency
-  console.log(JSON.stringify(values));
-  window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`);
+  console.log(window.location.origin + '/customers');
+  await axios.post(window.location.origin + '/customers', values)
+    .then(res => {
+      if (res.data.errors) {
+        let message = '';
+        for (const key in res.data.errors) {
+          message += `${key} ${res.data.errors[key].message} \n`;
+        }
+        alert(message);
+      } else {
+        window.location = '/'
+      }
+    });
 });
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,8 +3,12 @@ const router = Router();
 const User = require('../models/User');
 
 router.post('/customers', async (req, res) => {
-  const user = await new User(req.body).save();
-  res.send(user);
+  try{
+    const user = await new User(req.body).save();
+    res.send(user);
+  }catch(e){
+  	res.send(e);
+  }
 });
 
 module.exports = router;
