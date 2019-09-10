069eb6629ace12b166b2b68e9a01e335028c8e4f
Artrm Panasenko <artem253510@gmail.com>
2019-09-10 12:50:18 +0300

    layout cart v2

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -4,16 +4,10 @@ import './style.scss';
 
 export const HomePage = props => {
   return (
-<<<<<<< HEAD
-    <div className='home-page container'>
-            This is Home Page
-=======
     <div>
       <HomePageSlider />
       <div className="home-page container">
-        
       </div>
->>>>>>> development
     </div>
   );
 };
