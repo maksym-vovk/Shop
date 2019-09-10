1856ad495e8c7daaf1bfa7ab27c9e0e8dd014bc6
Artrm Panasenko <artem253510@gmail.com>
2019-09-10 14:55:17 +0300

    link for cart

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { Header, Footer } from './commons';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import store from './store';
-import { HomePage, IMac, MacBook, IPad, IPhone, Watch, RegisterForm } from './components';
+import { HomePage, IMac, MacBook, IPad, IPhone, Watch, RegisterForm, Cart } from './components';
 
 import './App.scss';
 
@@ -20,6 +20,7 @@ function App() {
             <Route path="/ipad" component={IPad} />
             <Route path="/iphone" component={IPhone} />
             <Route path="/watch" component={Watch} />
+            <Route path="/cart" component={Cart} />
             <Route path='/sig_in'/>
             <Route path="/sign_up" component={RegisterForm} />
           </Switch>
