3b2f9952a198177d9e4f257782561f3354b9bbe5
Maксим Вовк <mvx.vovk@gmail.com>
2019-09-11 19:26:36 +0300

    Search component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-
-import './App.scss';
 import { Header, Footer } from './commons';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import store from './store';
-import {HomePage, IMac, MacBook, IPad, IPhone, Watch, RegisterForm} from './components';
+import { HomePage, IMac, MacBook, IPad, IPhone, Watch, RegisterForm, Cart } from './components';
+import './App.scss';
 
 function App() {
   return (
