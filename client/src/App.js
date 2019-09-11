76496f893ead4fe2530f5ab7977ae1a78d630a6c
Maksym Vovk <53622336+maksym-vovk@users.noreply.github.com>
2019-09-11 19:45:27 +0300

    Search (#29)
    * Search
    
    * Search
    
    * Search component

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,6 @@ import { Header, Footer } from './commons';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import store from './store';
 import { HomePage, IMac, MacBook, IPad, IPhone, Watch, RegisterForm, Cart } from './components';
-
-import './App.scss';
-
 import './App.scss';
 
 function App() {
--- client/src/commons/Extras/index.js
+++ client/src/commons/Extras/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import './index.scss';
 import { Search, Cart, Auth } from '../';
 
