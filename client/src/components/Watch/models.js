b84ebf14508096fac834e3a5bf3ae2fa581dca45
kirsenkoDanIT <50655852+kirsenkoDanIT@users.noreply.github.com>
2019-09-12 15:41:26 +0300

    Product list basic layout (#32)
    * test
    
    * 12_cards
    
    * 6_cards
    
    * slider_img
    
    * slider_img
    
    * grid
    
    * main
    
    * routes
    
    * apple watch
    
    * apple watch
    
    * test product view
    
    * apple watch series 5
    
    * apple watch series 5
    
    * apple watch series 5

---
+++ client/src/components/Watch/models.js
@@ -0,0 +1,588 @@
+const pathBand = `/static/img/watch/Apple_Watch_Series_5/Space_Black_Stainless_Steel_Case_with_Leather_Loop`;
+const pathColors = `${pathBand}/colors`;
+// const path3 = `Space_Black_Stainless_Steel_Case_with_Space_Black_Milanese_Loop`;
+// const path4 = `Space_Black_Stainless_Steel_Case_with_Space_Black_Milanese_Loop`;
+// const path5 = `Space_Black_Stainless_Steel_Case_with_Space_Black_Milanese_Loop`;
+// const path6 = `Space_Black_Stainless_Steel_Case_with_Space_Black_Milanese_Loop`;
+// const path7 = `Space_Black_Stainless_Steel_Case_with_Space_Black_Milanese_Loop`;
+// const path8 = `Space_Black_Stainless_Steel_Case_with_Space_Black_Milanese_Loop`;
+
+// export const models = {
+//   model: {
+//     category: `series_5`,
+//     minPrice: `749`,
+//     filter: {
+//       model: `Apple Watch Series 5`,
+//       caseMaterial: `Stainless Steel`,
+//       caseFinish: `Space Black`,
+//       bandType: `Stainless Steel`
+//     },
+//     colors: {
+//       allColors: [
+//         {
+//           name: `Gold`,
+//           dataName: `Gold`,
+//           icon: `${pathColors}/Gold.jpg`
+//         },
+//         {
+//           name: `Silver`,
+//           dataName: `Silver`,
+//           icon: `${pathColors}/Silver.jpg`
+//         },
+//         {
+//           name: `Space Black`,
+//           dataName: `Space_Black`,
+//           icon: `${pathColors}/Space_Black.jpg`
+//         }
+//       ],
+//       bandImagesByColor: {
+//         Space_Black: [
+//           `${pathBand}/Space_Black/1.jpg`,
+//           `${pathBand}/Space_Black/2.jpg`,
+//           `${pathBand}/Space_Black/3.jpg`
+//         ],
+//         Silver: [
+//           `${pathBand}/Silver/1.jpg`,
+//           `${pathBand}/Silver/2.jpg`,
+//           `${pathBand}/Silver/3.jpg`
+//         ],
+//         Gold: [
+//           `${pathBand}/Gold/1.jpg`,
+//           `${pathBand}/Gold/2.jpg`,
+//           `${pathBand}/Gold/3.jpg`
+//         ]
+//       }
+//     },
+//     description: `Space Black Stainless Steel Case with Milanese Loop`,
+//     connectivity: {
+//       CELLULAR: {
+//         size_40_price: `749`,
+//         size_44_price: `799`
+//       }
+//     },
+//     techSpecs: {
+//       CELLULAR: {
+//         title: `GPS + Cellular`,
+//         body: [
+//           `Space Black stainless steel case`,
+//           `LTE and UMTS`,
+//           `Always-On Retina display with Force Touch`,
+//           `LTPO OLED display(1000 nits)`,
+//           `GPS/GNSS`,
+//           `Built-in Compass`,
+//           `S5 with 64-bit dual-core processor`,
+//           `W3 Apple wireless chip`,
+//           `Barometric altimeter`,
+//           `Optical heart sensor`,
+//           `Electrical heart sensor`,
+//           `Accelerometer up to 32 g-forces`,
+//           `Gyroscope`,
+//           `Improved ambient light sensor`,
+//           `Capacity 32GB`,
+//           `Digital Crown with haptic feedback`,
+//           `Speaker Ion-X strengthened glass`,
+//           `Sapphire crystal and ceramic back`,
+//           `Wi-Fi(802.11b/g/n 2.4GHz)`,
+//           `Bluetooth 5.0`,
+//           `Built-in rechargeable lithium-ion battery`,
+//           `Up to 18 hours of battery life`,
+//           `Water resistant 50 meters`,
+//           `watchOS 6`
+//         ]
+//       }
+//     },
+//     warehouseCount: 10,
+//     status: true
+//   }
+// };
+
+// export const models = {
+//   model: {
+//     category: `series_5`,
+//     minPrice: `799`,
+//     filter: {
+//       model: `Apple Watch Series 5`,
+//       caseMaterial: `Stainless Steel`,
+//       caseFinish: `Space Black`,
+//       bandType: `Leather`
+//     },
+//     colors: {
+//       allColors: [
+//         {
+//           name: `Black`,
+//           dataName: `Black`,
+//           icon: `${pathColors}/Black.jpg`
+//         },
+//         {
+//           name: `Meyer Lemon`,
+//           dataName: `Meyer_Lemon`,
+//           icon: `${pathColors}/Meyer_Lemon.jpg`
+//         },
+//         {
+//           name: `Saddle Brown`,
+//           dataName: `Saddle_Brown`,
+//           icon: `${pathColors}/Saddle_Brown.jpg`
+//         }
+//       ],
+//       bandImagesByColor: {
+//         Meyer_Lemon: [
+//           `${pathBand}/Meyer_Lemon/1.jpg`,
+//           `${pathBand}/Meyer_Lemon/2.jpg`,
+//           `${pathBand}/Meyer_Lemon/3.jpg`
+//         ],
+//         Black: [
+//           `${pathBand}/Black/1.jpg`,
+//           `${pathBand}/Black/2.jpg`,
+//           `${pathBand}/Black/3.jpg`
+//         ],
+//         Saddle_Brown: [
+//           `${pathBand}/Saddle_Brown/1.jpg`,
+//           `${pathBand}/Saddle_Brown/2.jpg`,
+//           `${pathBand}/Saddle_Brown/3.jpg`
+//         ]
+//       }
+//     },
+//     description: `Space Black Stainless Steel Case with Leather Loop`,
+//     connectivity: {
+//       CELLULAR: {
+//         size_44_price: `799`
+//       }
+//     },
+//     techSpecs: {
+//       CELLULAR: {
+//         title: `GPS + Cellular`,
+//         body: [
+//           `Space Black stainless steel case`,
+//           `LTE and UMTS`,
+//           `Always-On Retina display with Force Touch`,
+//           `LTPO OLED display(1000 nits)`,
+//           `GPS/GNSS`,
+//           `Built-in Compass`,
+//           `S5 with 64-bit dual-core processor`,
+//           `W3 Apple wireless chip`,
+//           `Barometric altimeter`,
+//           `Optical heart sensor`,
+//           `Electrical heart sensor`,
+//           `Accelerometer up to 32 g-forces`,
+//           `Gyroscope`,
+//           `Improved ambient light sensor`,
+//           `Capacity 32GB`,
+//           `Digital Crown with haptic feedback`,
+//           `Speaker Ion-X strengthened glass`,
+//           `Sapphire crystal and ceramic back`,
+//           `Wi-Fi(802.11b/g/n 2.4GHz)`,
+//           `Bluetooth 5.0`,
+//           `Built-in rechargeable lithium-ion battery`,
+//           `Up to 18 hours of battery life`,
+//           `Water resistant 50 meters`,
+//           `watchOS 6`
+//         ]
+//       }
+//     },
+//     warehouseCount: 10,
+//     status: true
+//   }
+// };
+
+// export const models = {
+//   model: {
+//     category: `series_5`,
+//     minPrice: `799`,
+//     filter: {
+//       model: `Apple Watch Series 5`,
+//       caseMaterial: `Stainless Steel`,
+//       caseFinish: `Space Black`,
+//       bandType: `Leather`
+//     },
+//     colors: {
+//       allColors: [
+//         {
+//           name: `Black`,
+//           dataName: `Black`,
+//           icon: `${pathColors}/Black.jpg`
+//         },
+//         {
+//           name: `Meyer Lemon`,
+//           dataName: `Meyer_Lemon`,
+//           icon: `${pathColors}/Meyer_Lemon.jpg`
+//         },
+//         {
+//           name: `Saddle Brown`,
+//           dataName: `Saddle_Brown`,
+//           icon: `${pathColors}/Saddle_Brown.jpg`
+//         }
+//       ],
+//       bandImagesByColor: {
+//         Camel: [
+//           `${pathBand}/Black/1.jpg`,
+//           `${pathBand}/Black/2.jpg`,
+//           `${pathBand}/Black/3.jpg`
+//         ],
+//         Pomegranate: [
+//           `${pathBand}/Meyer_Lemon/1.jpg`,
+//           `${pathBand}/Meyer_Lemon/2.jpg`,
+//           `${pathBand}/Meyer_Lemon/3.jpg`
+//         ],
+//         Khaki: [
+//           `${pathBand}/Saddle_Brown/1.jpg`,
+//           `${pathBand}/Saddle_Brown/2.jpg`,
+//           `${pathBand}/Saddle_Brown/3.jpg`
+//         ]
+//       }
+//     },
+//     description: `Space Black Stainless Steel Case with Leather Loop`,
+//     connectivity: {
+//       CELLULAR: {
+//         size_44_price: `799`
+//       }
+//     },
+//     techSpecs: {
+//       CELLULAR: {
+//         title: `GPS + Cellular`,
+//         body: [
+//           `Space Black stainless steel case`,
+//           `LTE and UMTS`,
+//           `Always-On Retina display with Force Touch`,
+//           `LTPO OLED display(1000 nits)`,
+//           `GPS/GNSS`,
+//           `Built-in Compass`,
+//           `S5 with 64-bit dual-core processor`,
+//           `W3 Apple wireless chip`,
+//           `Barometric altimeter`,
+//           `Optical heart sensor`,
+//           `Electrical heart sensor`,
+//           `Accelerometer up to 32 g-forces`,
+//           `Gyroscope`,
+//           `Improved ambient light sensor`,
+//           `Capacity 32GB`,
+//           `Digital Crown with haptic feedback`,
+//           `Speaker Ion-X strengthened glass`,
+//           `Sapphire crystal and ceramic back`,
+//           `Wi-Fi(802.11b/g/n 2.4GHz)`,
+//           `Bluetooth 5.0`,
+//           `Built-in rechargeable lithium-ion battery`,
+//           `Up to 18 hours of battery life`,
+//           `Water resistant 50 meters`,
+//           `watchOS 6`
+//         ]
+//       }
+//     },
+//     warehouseCount: 10,
+//     status: true
+//   }
+// };
+
+// export const models = {
+//   model: {
+//     category: `series_5`,
+//     minPrice: `699`,
+//     filter: {
+//       model: `Apple Watch Series 5`,
+//       caseMaterial: `Stainless Steel`,
+//       caseFinish: `Silver`,
+//       bandType: `Sport Loop`
+//     },
+//     colors: {
+//       allColors: [
+//         {
+//           name: `Alaskan Blue`,
+//           dataName: `Alaskan_Blue`,
+//           icon: `${pathColors}/Alaskan_Blue.jpg`
+//         },
+//         {
+//           name: `Anchor Gray`,
+//           dataName: `Anchor_Gray`,
+//           icon: `${pathColors}/Anchor_Gray.jpg`
+//         },
+//         {
+//           name: `Camel`,
+//           dataName: `Camel`,
+//           icon: `${pathColors}/Camel.jpg`
+//         },
+//         {
+//           name: `Khaki`,
+//           dataName: `Khaki`,
+//           icon: `${pathColors}/Khaki.jpg`
+//         },
+//         {
+//           name: `Midnight Blue`,
+//           dataName: `Midnight_Blue`,
+//           icon: `${pathColors}/Midnight_Blue.jpg`
+//         },
+//         {
+//           name: `Pomegranate`,
+//           dataName: `Pomegranate`,
+//           icon: `${pathColors}/Pomegranate.jpg`
+//         },
+//         {
+//           name: `Pride`,
+//           dataName: `Pride`,
+//           icon: `${pathColors}/Pride.jpg`
+//         }
+//       ],
+//       bandImagesByColor: {
+//         Camel: [
+//           `${pathBand}/Camel/1.jpg`,
+//           `${pathBand}/Camel/2.jpg`,
+//           `${pathBand}/Camel/3.jpg`
+//         ],
+//         Pomegranate: [
+//           `${pathBand}/Pomegranate/1.jpg`,
+//           `${pathBand}/Pomegranate/2.jpg`,
+//           `${pathBand}/Pomegranate/3.jpg`
+//         ],
+//         Khaki: [
+//           `${pathBand}/Khaki/1.jpg`,
+//           `${pathBand}/Khaki/2.jpg`,
+//           `${pathBand}/Khaki/3.jpg`
+//         ],
+
+//         Anchor_Gray: [
+//           `${pathBand}/Anchor_Gray/1.jpg`,
+//           `${pathBand}/Anchor_Gray/2.jpg`,
+//           `${pathBand}/Anchor_Gray/3.jpg`
+//         ],
+//         Alaskan_Blue: [
+//           `${pathBand}/Alaskan_Blue/1.jpg`,
+//           `${pathBand}/Alaskan_Blue/2.jpg`,
+//           `${pathBand}/Alaskan_Blue/3.jpg`
+//         ],
+
+//         Midnight_Blue: [
+//           `${pathBand}/Midnight_Blue/1.jpg`,
+//           `${pathBand}/Midnight_Blue/2.jpg`,
+//           `${pathBand}/Midnight_Blue/3.jpg`
+//         ],
+
+//         Pride: [
+//           `${pathBand}/Pride/1.jpg`,
+//           `${pathBand}/Pride/2.jpg`,
+//           `${pathBand}/Pride/3.jpg`
+//         ]
+//       }
+//     },
+//     description: `Stainless Steel Case with Sport Loop`,
+//     connectivity: {
+//       CELLULAR: {
+//         size_40_price: `699`,
+//         size_44_price: `749`
+//       }
+//     },
+//     techSpecs: {
+//       CELLULAR: {
+//         title: `GPS + Cellular`,
+//         body: [
+//           `Stainless steel case`,
+//           `LTE and UMTS`,
+//           `Always-On Retina display with Force Touch`,
+//           `LTPO OLED display(1000 nits)`,
+//           `GPS/GNSS`,
+//           `Built-in Compass`,
+//           `S5 with 64-bit dual-core processor`,
+//           `W3 Apple wireless chip`,
+//           `Barometric altimeter`,
+//           `Optical heart sensor`,
+//           `Electrical heart sensor`,
+//           `Accelerometer up to 32 g-forces`,
+//           `Gyroscope`,
+//           `Improved ambient light sensor`,
+//           `Capacity 32GB`,
+//           `Digital Crown with haptic feedback`,
+//           `Speaker Ion-X strengthened glass`,
+//           `Sapphire crystal and ceramic back`,
+//           `Wi-Fi(802.11b/g/n 2.4GHz)`,
+//           `Bluetooth 5.0`,
+//           `Built-in rechargeable lithium-ion battery`,
+//           `Up to 18 hours of battery life`,
+//           `Water resistant 50 meters`,
+//           `watchOS 6`
+//         ]
+//       }
+//     },
+//     warehouseCount: 10,
+//     status: true
+//   }
+// };
+
+// export const models = {
+//   model: {
+//     category: `series_5`,
+//     minPrice: `399`,
+//     filter: {
+//       model: `Apple Watch Series 5`,
+//       caseMaterial: `Aluminum`,
+//       caseFinish: `Space Gray`,
+//       bandType: `Sport Band`
+//     },
+//     colors: {
+//       allColors: [
+//         {
+//           name: `Alaskan Blue`,
+//           dataName: `Alaskan_Blue`,
+//           icon: `${pathColors}/Alaskan_Blue.jpg`
+//         },
+//         {
+//           name: `Black`,
+//           dataName: `Black`,
+//           icon: `${pathColors}/Black.jpg`
+//         },
+//         {
+//           name: `Clementine`,
+//           dataName: `Clementine`,
+//           icon: `${pathColors}/Clementine.jpg`
+//         },
+//         {
+//           name: `Lemon Cream`,
+//           dataName: `Lemon_Cream`,
+//           icon: `${pathColors}/Lemon_Cream.jpg`
+//         },
+//         {
+//           name: `Pine Green`,
+//           dataName: `Pine_Green`,
+//           icon: `${pathColors}/Pine_Green.jpg`
+//         },
+//         {
+//           name: `Pink Sand`,
+//           dataName: `Pink_Sand`,
+//           icon: `${pathColors}/Pink_Sand.jpg`
+//         },
+//         {
+//           name: `Red`,
+//           dataName: `Red`,
+//           icon: `${pathColors}/Red.jpg`
+//         },
+//         {
+//           name: `Stone`,
+//           dataName: `Stone`,
+//           icon: `${pathColors}/Stone.jpg`
+//         },
+//         {
+//           name: `White`,
+//           dataName: `White`,
+//           icon: `${pathColors}/colors/White.jpg`
+//         }
+//       ],
+//       bandImagesByColor: {
+//         Red: [
+//           `${pathBand}/Red/1.jpg`,
+//           `${pathBand}/Red/2.jpg`,
+//           `${pathBand}/Red/3.jpg`
+//         ],
+//         White: [
+//           `${pathBand}/White/1.jpg`,
+//           `${pathBand}/White/2.jpg`,
+//           `${pathBand}/White/3.jpg`
+//         ],
+//         Alaskan_Blue: [
+//           `${pathBand}/Alaskan_Blue/1.jpg`,
+//           `${pathBand}/Alaskan_Blue/2.jpg`,
+//           `${pathBand}/Alaskan_Blue/3.jpg`
+//         ],
+
+//         Stone: [
+//           `${pathBand}/Stone/1.jpg`,
+//           `${pathBand}/Stone/2.jpg`,
+//           `${pathBand}/Stone/3.jpg`
+//         ],
+
+//         Pink_Sand: [
+//           `${pathBand}/Pink_Sand/1.jpg`,
+//           `${pathBand}/Pink_Sand/2.jpg`,
+//           `${pathBand}/Pink_Sand/3.jpg`
+//         ],
+//         Pine_Green: [
+//           `${pathBand}/Pine_Green/1.jpg`,
+//           `${pathBand}/Pine_Green/2.jpg`,
+//           `${pathBand}/Pine_Green/3.jpg`
+//         ],
+//         Lemon_Cream: [
+//           `${pathBand}/Lemon_Cream/1.jpg`,
+//           `${pathBand}/Lemon_Cream/2.jpg`,
+//           `${pathBand}/Lemon_Cream/3.jpg`
+//         ],
+//         Clementine: [
+//           `${pathBand}/Clementine/1.jpg`,
+//           `${pathBand}/Clementine/2.jpg`,
+//           `${pathBand}/Clementine/3.jpg`
+//         ],
+//         Black: [
+//           `${pathBand}/Black/1.jpg`,
+//           `${pathBand}/Black/2.jpg`,
+//           `${pathBand}/Black/3.jpg`
+//         ]
+//       }
+//     },
+//     description: `Gold Aluminum Case with Sport Band`,
+//     connectivity: {
+//       GPS: {
+//         size_40_price: `399`,
+//         size_44_price: `429`
+//       },
+//       CELLULAR: {
+//         size_40_price: `499`,
+//         size_44_price: `529`
+//       }
+//     },
+//     techSpecs: {
+//       GPS: {
+//         title: `GPS`,
+//         body: [
+//           `Gold aluminum case`,
+//           `Always-On Retina display with Force Touch`,
+//           `LTPO OLED display(1000 nits)`,
+//           `GPS/GNSS`,
+//           `Built-in Compass`,
+//           `S5 with 64-bit dual-core processor`,
+//           `W3 Apple wireless chip`,
+//           `Barometric altimeter`,
+//           `Optical heart sensor`,
+//           `Electrical heart sensor`,
+//           `Accelerometer up to 32 g-forces`,
+//           `Gyroscope`,
+//           `Improved ambient light sensor`,
+//           `Capacity 32GB`,
+//           `Digital Crown with haptic feedback`,
+//           `Speaker Ion-X strengthened glass`,
+//           `Sapphire crystal and ceramic back`,
+//           `Wi-Fi(802.11b/g/n 2.4GHz)`,
+//           `Bluetooth 5.0`,
+//           `Built-in rechargeable lithium-ion battery`,
+//           `Up to 18 hours of battery life`,
+//           `Water resistant 50 meters`,
+//           `watchOS 6`
+//         ]
+//       },
+//       CELLULAR: {
+//         title: `GPS + Cellular`,
+//         body: [
+//           `Gold aluminum case`,
+//           `LTE and UMTS`,
+//           `Always-On Retina display with Force Touch`,
+//           `LTPO OLED display(1000 nits)`,
+//           `GPS/GNSS`,
+//           `Built-in Compass`,
+//           `S5 with 64-bit dual-core processor`,
+//           `W3 Apple wireless chip`,
+//           `Barometric altimeter`,
+//           `Optical heart sensor`,
+//           `Electrical heart sensor`,
+//           `Accelerometer up to 32 g-forces`,
+//           `Gyroscope`,
+//           `Improved ambient light sensor`,
+//           `Capacity 32GB`,
+//           `Digital Crown with haptic feedback`,
+//           `Speaker Ion-X strengthened glass`,
+//           `Sapphire crystal and ceramic back`,
+//           `Wi-Fi(802.11b/g/n 2.4GHz)`,
+//           `Bluetooth 5.0`,
+//           `Built-in rechargeable lithium-ion battery`,
+//           `Up to 18 hours of battery life`,
+//           `Water resistant 50 meters`,
+//           `watchOS 6`
+//         ]
+//       }
+//     },
+//     warehouseCount: 10,
+//     status: true
+//   }
+// };
