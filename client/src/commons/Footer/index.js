00d6aa7455276198c6e7527e84808c501ebcbf10
Artrm Panasenko <artem253510@gmail.com>
2019-09-12 15:21:49 +0300

    footter fix bags

--- client/src/commons/Footer/index.js
+++ client/src/commons/Footer/index.js
@@ -9,40 +9,43 @@ export const Footer = () => {
       <div className='container footer__container'>
         <div className='section'>
           <div className='section__item'>
-            <h4 className='section__item--title'>Menu</h4>
-            <ul>
-              <li><Link to="/all">All Watch</Link></li>
-              <li><Link to="/origin">Origin Watch</Link></li>
-              <li><Link to="/nike">Nike Watch</Link></li>
-              <li><Link to="/hermes">Hermès Watch</Link></li>
-              <li><Link to="/edition">Edition Watch</Link></li>
-            </ul>
-          </div>
-          <div className='section__item section__product'>
-            <h4 className='section__item--title'>Information</h4>
-            <ul>
-              <li><Link to='/'>About Us</Link></li>
-              <li><Link to='/'>Website Terms of Use</Link></li>
-              <li><Link to='/'>Jobs</Link></li>
-              <li><Link to='/'>Contacts</Link></li>
-            </ul>
+            <div>
+              <h4 className='section__item--title'>Menu</h4>
+              <ul>
+                <li><Link to="/all">All Watch</Link></li>
+                <li><Link to="/origin">Origin Watch</Link></li>
+                <li><Link to="/nike">Nike Watch</Link></li>
+                <li><Link to="/hermes">Hermès Watch</Link></li>
+                <li><Link to="/edition">Edition Watch</Link></li>
+              </ul>
+            </div>
           </div>
           <div className='section__item'>
-            <h4 className='section__item--title'>Help</h4>
-            <ul>
-              <li><Link to='/'>Shipping and payment</Link></li>
-              <li><Link to='/'>Warranty</Link></li>
-              <li><Link to='/'>Purchase returns</Link></li>
-              <li><Link to='/'>Service centres</Link></li>
-              <li><Link to='/'>Questions and answers</Link></li>
-            </ul>
+            <div>
+              <h4 className='section__item--title'>Help</h4>
+              <ul>
+                <li><Link to='/'>Shipping and payment</Link></li>
+                <li><Link to='/'>Warranty</Link></li>
+                <li><Link to='/'>Purchase returns</Link></li>
+                <li><Link to='/'>Service centres</Link></li>
+                <li><Link to='/'>Questions and answers</Link></li>
+              </ul>
+            </div>
+          </div>
+          <div className='section__item section__product'>
+            <div>
+              <h4 className='section__item--title'>Information</h4>
+              <ul>
+                <li><Link to='/'>About Us</Link></li>
+                <li><Link to='/'>Terms of Use</Link></li>
+                <li><Link to='/'>Jobs</Link></li>
+                <li><Link to='/'>Contacts</Link></li>
+              </ul>
+            </div>
           </div>
         </div>
-        <span className='footer__retailer footer__sub-text'>
-                More ways to shop: Find an Apple Store or other retailer near you. Or call 1-800-MY-APPLE.
-        </span>
         <span className='footer__copyright footer__sub-text'>
-                Copyright © 2019 Apple Inc. All rights reserved.
+                Copyright © 2019. All rights reserved.
         </span>
       </div>
     </footer>
