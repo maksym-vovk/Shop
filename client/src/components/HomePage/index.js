541d547e51c61dc27bb6b18a78ff47749ddf6c9f
Ivan Kravchenko <aivan.kr@gmail.com>
2019-09-15 10:45:26 +0300

    fetch products to store (#42)

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { HomePageSlider, ProductsList } from '../';
+
+import { fetchCards } from '../../store';
+
 import './style.scss';
 
-export const HomePage = props => {
+export const HomePage = connect(null, { fetchCards })(props => {
+  props.fetchCards();
   return (
     <div>
       <HomePageSlider />
       <ProductsList path="/watch" slice="5" />
     </div>
   );
-};
+});
