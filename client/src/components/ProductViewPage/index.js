75e42e968e66149a56241087b1b159bf5195ff81
kirsenkoDanIT <a.kirs@ukr.net>
2019-09-18 01:38:43 +0300

    1

--- client/src/components/ProductViewPage/index.js
+++ client/src/components/ProductViewPage/index.js
@@ -16,134 +16,75 @@ export const ProductViewPage = connect(
 )(props => {
   const { card, fetchCard } = props;
   const [loading, setLoading] = useState(true);
-  console.log(props);
+  const [images, setImages] = useState();
+
   useEffect(() => {
     fetchCard(props.match.params.id);
     setLoading(false);
-  }, [fetchCard]);
+  }, [fetchCard, props.match.params.id]);
+
   console.log('card', card);
-  const Card = () => {
-    return card ? <img
-      className="product-card__image"
-      src={Object.entries(card[0].colors.bandImagesByColor)[0][1][0]}
-      alt="apple-watch"
-    /> : null;
+  // console.log('images', images);
+  console.log('props', props);
+
+  const Slider = () => {
+    return (
+      images || Object.entries(card[0].colors.bandImagesByColor)[0][1]
+    ).map((item, index) => {
+      return (
+        <img src={item} key={index} alt="watch" style={{ width: '50%' }} />
+      );
+    });
+  };
+
+  const tabClickHandler = e => {
+    // console.log(e.target.dataset.name)
+    const data = e.target.dataset.name;
+    console.log(data);
+    setImages(card[0].colors.bandImagesByColor[data]);
+  };
+
+  const ColorTabs = () => {
+    return (
+      <div>
+        {card[0].colors.allColors.map((item, index) => {
+          return (
+            <span key={index} onClick={tabClickHandler}>
+              <img
+                src={item.icon}
+                alt={item.name}
+                title={item.name}
+                data-name={item.dataName}
+              />
+            </span>
+          );
+        })}
+      </div>
+    );
   };
 
   return (
     <React.Fragment>
-      <section className="product-view">
-        <Card/>
-      </section>
-      <Lines customLoading={loading} time={500} />;
+      {card ? (
+        <section className="product-view" style={{ padding: '10px 0' }}>
+          <div className="container">
+            <h2 style={{ textAlign: 'left' }}>{card[0].filter.model}</h2>
+          </div>
+          <hr style={{ opacity: '0.5' }} />
+          <article className="product-view__item   container">
+            <div style={{width:'100%'}}>
+              <Slider />
+            </div>
+            <div style={{ padding: '20px 0' }}>
+              <h2>{card[0].description}</h2>
+              <p>${card[0].minPrice}</p>
+              <p>Band colors</p>
+              <ColorTabs />
+            </div>
+          </article>
+        </section>
+      ) : null}
+      <Lines customLoading={loading} time={500} />
     </React.Fragment>
   );
 });
-
-// export const ProductViewPage = props => {
-//   // console.log('props', props);
-//   const [data, setData] = useState();
-//   const [mainImg, setMainImg] = useState();
-
-//   const fetchData = async id => {
-//     const result = await axios(`/cards/${id}`);
-//     setData(result.data[0]);
-//   };
-
-//   const res = {
-//     params: {
-//       // 'filter.category': 'watch',
-//       // 'filter.size': '40mm',
-//       // 'filter.bandType': 'Sport',
-//       // 'filter.bandColor': 'white',
-//       // 'filter.caseMaterial': 'Aluminium',
-//     }
-//   };
-
-//   const getDataAxios = async res => {
-//     const response = await axios.get('/cards', res);
-//     console.log('response', response.data);
-//   };
-
-//   // getDataAxios(res);
-
-//   useEffect(() => {
-//     fetchData(props.match.params.id);
-//   }, [props.match.params.id]);
-//   // console.log('data', data);
-
-//   const tabsHandler = e => {
-//     // console.log(e.target.dataset.src);
-//     setMainImg(e.target.dataset.src);
-//   };
-
-//   const Tabs = () => {
-//     return data ? (
-//       <div className="tabs-images">
-//         {data.images.tabsImages.map((item, index) => {
-//           return (
-//             <img
-//               key={index}
-//               alt={data.model}
-//               onMouseEnter={tabsHandler}
-//               data-src={item}
-//               style={{
-//                 width: '10%'
-//               }}
-//               src={item}
-//             />
-//           );
-//         })}
-//       </div>
-//     ) : null;
-//   };
-//   let flag = false;
-//   let flag2 = false;
-//   return (
-//     <section className="product-view">
-//       <button
-//         onClick={() => {
-//           flag
-//             ? delete res.params['filter.size']
-//             : (res.params['filter.size'] = '40mm');
-//           console.log(flag);
-//           getDataAxios(res);
-//           flag = !flag;
-//         }}
-//       >
-//         size
-//       </button>
-//       <button
-//         onClick={() => {
-//           flag2
-//             ? delete res.params['filter.bandType']
-//             : (res.params['filter.bandType'] = 'Sport');
-//           console.log(res);
-//           getDataAxios(res);
-//           flag2 = !flag2;
-//         }}
-//       >
-//         band
-//       </button>
-//       <article className="product-view-wrapper container">
-//         <div className="product-view__images">
-//           {data ? (
-//             <div className="main-image">
-//               <img
-//                 alt={data.model}
-//                 style={{
-//                   width: '70%'
-//                 }}
-//                 src={mainImg || data.images.tabsImages[0]}
-//               />
-//             </div>
-//           ) : null}
-//           <Tabs />
-//         </div>
-//         <div className="product-view__description">
-//           {data ? <h2>{data.description}</h2> : null}
-//         </div>
-//       </article>
-//     </section>
-//   );
-// };
