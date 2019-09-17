6ae7849bc235368e1a841014f85cafc9dc647cd9
kirsenkoDanIT <50655852+kirsenkoDanIT@users.noreply.github.com>
2019-09-17 14:10:23 +0300

    Cards to store (#47)
    * 1
    
    * 1
    
    * fetch products to store (#42)
    
    * 1
    
    * saga

---
+++ client/src/store/reducers.js
@@ -0,0 +1,71 @@
+import { combineReducers } from 'redux';
+import { reducer as reduxFormReducer } from 'redux-form';
+import * as ATYPES from './constants.js';
+// state for start
+const initialState = {
+  authData: {
+    authorized: false
+  },
+
+  // Search
+  searchStatus: {
+    status: false
+  },
+
+  // Fetch
+
+  products: {
+    cards: []
+  }
+};
+
+function authReducer(state = initialState.authData, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case ATYPES.SET_AUTHORIZED:
+      return {
+        ...state,
+        authorized: payload
+      };
+    default:
+      return state;
+  }
+}
+// Search
+function searchReducer(state = initialState.searchStatus, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case ATYPES.SET_SEARCH_STATUS:
+      return {
+        ...state,
+        status: payload
+      };
+    default:
+      return state;
+  }
+}
+// Fetch
+function fetchReducer(state = initialState.products, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case ATYPES.SET_CARDS:
+      return {
+        ...state,
+        cards: payload
+      };
+    case ATYPES.SET_CARD:
+      return {
+        ...state,
+        card: payload
+      };
+    default:
+      return state;
+  }
+}
+
+export const reducer = combineReducers({
+  form: reduxFormReducer, // mounted under "form"
+  authorization: authReducer,
+  searchStatus: searchReducer,
+  products: fetchReducer
+});
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -15,20 +15,7 @@ router.get('/cards/:id', async (req, res) => {
   });
   res.json(product);
 });
-router.get('/phones', async (req, res) => {
-  const products = await Product.find({
-    category: 'phone'
-  });
-  res.json(products);
-});
 
-router.get('/watch', async (req, res) => {
-  const products = await Product.find({
-    'filter.category': 'watch',
-    'filter.size': '40mm'
-  });
-  res.json(products);
-});
 
 router.post('/cards', async (req, res) => {
   try {
