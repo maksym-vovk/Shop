bddda1907b10dd60852356ea54845d15b499e7c4
kirsenkoDanIT <a.kirs@ukr.net>
2019-09-21 21:59:42 +0300

    9

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
+import { Lines } from 'react-preloaders';
 import { HomePageSlider } from '../';
 
 import './style.scss';
 
 export const HomePage = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(false);
+  }, []);
   return (
     <div>
       <HomePageSlider />
+      <Lines customLoading={loading} time={300} />
     </div>
   );
 };
