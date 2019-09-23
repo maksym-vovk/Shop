8ef23fa2cbca5b0e0d0a0901bb05a62b49d3f911
kirsenkoDanIT <a.kirs@ukr.net>
2019-09-23 14:13:33 +0300

    filter preview classes

--- client/src/components/Filter/index.js
+++ client/src/components/Filter/index.js
@@ -9,42 +9,42 @@ export const Filter = () => {
     <React.Fragment>
       <Collapsible trigger="Filter">
         <section className="filter-wrapper">
-          <p>Reset</p>
-          <article>
-            <h3>Model</h3>
-            <ul>
-              <li>Apple Watch</li>
-              <li>Apple Watch Nike</li>
-              <li>Apple Watch Hermes</li>
-              <li>Apple Watch Edition</li>
+          <button className='filter-reset-btn'>Reset</button>
+          <article className='filter-block'>
+            <h3 className='filter-block__title'>Model</h3>
+            <ul className='filter-block__items-wrapper'>
+              <li className='filter-block__item'>Apple Watch</li>
+              <li className='filter-block__item'>Apple Watch Nike</li>
+              <li className='filter-block__item'>Apple Watch Hermes</li>
+              <li className='filter-block__item'>Apple Watch Edition</li>
             </ul>
           </article>
-          <article>
-            <h3>Case Material</h3>
-            <ul>
-              <li>Aluminum</li>
-              <li>Stainless Steel</li>
-              <li>Titanium</li>
-              <li>Ceramic</li>
+          <article className='filter-block'>
+            <h3 className='filter-block__title'>Case Material</h3>
+            <ul className='filter-block__items-wrapper'>
+              <li className='filter-block__item'>Aluminum</li>
+              <li className='filter-block__item'>Stainless Steel</li>
+              <li className='filter-block__item'>Titanium</li>
+              <li className='filter-block__item'>Ceramic</li>
             </ul>
           </article>
-          <article>
-            <h3>Case Finish</h3>
-            <ul>
-              <li>Space Gray</li>
-              <li>Silver</li>
-              <li>Gold</li>
-              <li>Space Black</li>
-              <li>White</li>
+          <article className='filter-block'>
+            <h3 className='filter-block__title'>Case Finish</h3>
+            <ul className='filter-block__items-wrapper'>
+              <li className='filter-block__item'>Space Gray</li>
+              <li className='filter-block__item'>Silver</li>
+              <li className='filter-block__item'>Gold</li>
+              <li className='filter-block__item'>Space Black</li>
+              <li className='filter-block__item'>White</li>
             </ul>
           </article>
-          <article>
-            <h3>Band Type</h3>
-            <ul>
-              <li>Sport Band</li>
-              <li>Sport Loop</li>
-              <li>Leather</li>
-              <li>Stainless Steel</li>
+          <article className='filter-block'>
+            <h3 className='filter-block__title'>Band Type</h3>
+            <ul className='filter-block__items-wrapper'>
+              <li className='filter-block__item'>Sport Band</li>
+              <li className='filter-block__item'>Sport Loop</li>
+              <li className='filter-block__item'>Leather</li>
+              <li className='filter-block__item'>Stainless Steel</li>
             </ul>
           </article>
         </section>
