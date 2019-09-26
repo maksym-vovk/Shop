269dd76ca121c992a59a1939b76778e6faa17c7b
kirsenkoDanIT <50655852+kirsenkoDanIT@users.noreply.github.com>
2019-09-26 14:34:18 +0300

    Filter (#70)
    * 1
    
    * 2
    
    * 2
    
    * 3
    
    * 5
    
    * 6
    
    * 7
    
    * 8
    
    * 9
    
    * 10
    
    * 11
    
    * footer_bottom
    
    * header-heigth
    
    * favicon and category titles
    
    * favicon and category titles
    
    * cosoleLog
    
    * listPadding
    
    * previewCarousel
    
    * titleCard
    
    * filter preview
    
    * filter preview classes
    
    * filter preview styled
    
    * filter params
    
    * filter works
    
    * filter works
    
    * scss
    
    * pull
    
    * params
    
    * params
    
    * status
    
    * pull
    
    * console_log
    
    * pull

--- client/src/components/All/index.js
+++ client/src/components/All/index.js
@@ -1,14 +1,14 @@
-import React, {useEffect} from 'react';
-import { ProductsList } from '../ProductsList';
+import React, { useEffect } from 'react';
+import { Filter } from '../';
 
 export const All = () => {
   useEffect(() => {
     document.title = 'Apple Watch Series 5';
-  }, [])
+  }, []);
   return (
     <section className="container">
-      <h2 className='page-title'>Apple Watch Series 5</h2>
-      <ProductsList />
+      <h2 className="page-title">Apple Watch Series 5</h2>
+      <Filter />
     </section>
   );
 };
