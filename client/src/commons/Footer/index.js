2124c57a8baa272815c2cb51391c3de7905a6d05
Artrm Panasenko <artem253510@gmail.com>
2019-09-30 14:44:14 +0300

    footer_adaptive

--- client/src/commons/Footer/index.js
+++ client/src/commons/Footer/index.js
@@ -12,11 +12,11 @@ export const Footer = () => {
             <div>
               <h4 className='section__item--title'>Menu</h4>
               <ul>
-                <li><Link to="/all">All Watch</Link></li>
-                <li><Link to="/origin">Origin Watch</Link></li>
-                <li><Link to="/nike">Nike Watch</Link></li>
-                <li><Link to="/hermes">Hermès Watch</Link></li>
-                <li><Link to="/edition">Edition Watch</Link></li>
+                <li><Link className='link' className = 'link' to="/all">All Watch</Link></li>
+                <li><Link className='link' to="/origin">Origin Watch</Link></li>
+                <li><Link className='link' to="/nike">Nike Watch</Link></li>
+                <li><Link className='link' to="/hermes">Hermès Watch</Link></li>
+                <li><Link className='link' to="/edition">Edition Watch</Link></li>
               </ul>
             </div>
           </div>
@@ -24,11 +24,11 @@ export const Footer = () => {
             <div>
               <h4 className='section__item--title'>Help</h4>
               <ul>
-                <li><Link to='/'>Shipping and payment</Link></li>
-                <li><Link to='/'>Warranty</Link></li>
-                <li><Link to='/'>Purchase returns</Link></li>
-                <li><Link to='/'>Service centres</Link></li>
-                <li><Link to='/'>Questions and answers</Link></li>
+                <li><Link className='link' to='/'>Shipping and payment</Link></li>
+                <li><Link className='link' to='/'>Warranty</Link></li>
+                <li><Link className='link' to='/'>Purchase returns</Link></li>
+                <li><Link className='link' to='/'>Service centres</Link></li>
+                <li><Link className='link' to='/'>Questions and answers</Link></li>
               </ul>
             </div>
           </div>
@@ -36,10 +36,10 @@ export const Footer = () => {
             <div>
               <h4 className='section__item--title'>Information</h4>
               <ul>
-                <li><Link to='/'>About Us</Link></li>
-                <li><Link to='/'>Terms of Use</Link></li>
-                <li><Link to='/'>Jobs</Link></li>
-                <li><Link to='/'>Contacts</Link></li>
+                <li><Link className='link' to='/'>About Us</Link></li>
+                <li><Link className='link' to='/'>Terms of Use</Link></li>
+                <li><Link className='link' to='/'>Jobs</Link></li>
+                <li><Link className='link' to='/'>Contacts</Link></li>
               </ul>
             </div>
           </div>
