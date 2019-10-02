642e79483fc42d51723d9f0a366dc48d4714d23c
Maксим Вовк <mvx.vovk@gmail.com>
2019-10-02 15:15:21 +0300

    'ProductSyles'

--- client/src/components/Filter/index.js
+++ client/src/components/Filter/index.js
@@ -70,10 +70,14 @@ export const Filter = () => {
     <React.Fragment>
       <Collapsible trigger="Filter">
         <section className="filter">
-          <button className="filter__reset-btn" onClick={resetHandler}>
-            Reset
-          </button>
-          {filterTabs}
+          <div className='filter__reset'>
+            <button className="filter__reset-btn" onClick={resetHandler}>
+              Reset
+            </button>
+          </div>
+          <div className="filter__tabs">
+            {filterTabs}
+          </div>
         </section>
       </Collapsible>
       <ProductsList params={res} key={key} />
