18e04e846b296a045844aafe4932d305821e7ee2
irina-kavun <kavunirina@gmail.com>
2019-10-02 20:23:15 +0300

    header fixed plus burger added

--- client/src/commons/Header/index.js
+++ client/src/commons/Header/index.js
@@ -1,18 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { connect } from 'react-redux';
+import {Collapse} from 'react-collapse'
+
 import { Logo, MainMenu, Extras, SearchInput } from '../';
+
 import './index.scss';
 
 const mapStateToProps = state => ({
   status: state.search.searchStatus
 });
 
+// const collapse = () => {
+//     const menu = document.querySelector('.main-menu');
+//     const header = document.querySelector('.header');
+//
+//     if (menu.style.maxHeight) {
+//         menu.style.maxHeight = null;
+//         setTimeout(() => {header.style.overflow = 'hidden';}, 1000)
+//     }
+//     else {
+//         header.style.overflow = 'unset';
+//         // menu.style.maxHeight = menu.scrollHeight + 'vh';
+//         menu.style.maxHeight = 100 + 'vh';
+//         menu.style.height = 100 + 'vh';
+//     }
+// }
+
+const Test = () => {
+
+    return (
+        <React.Fragment>
+
+
+        </React.Fragment>
+    )
+}
 export const Header = connect(mapStateToProps)(props => {
-  return (
-    <header className="page-header">
-      <div className="page-header__wrapper container">
-        <Logo />
-        {props.status ? <SearchInput /> : <MainMenu />}
+    const [isOpened, openStatus] = useState(false);
+
+
+    return (
+    <header className="header">
+      <div className="header__wrapper container">
+
+          <button type="button" onClick={() => openStatus(!isOpened)} className="test">burger</button>
+
+          <Collapse className="collapse-menu" isOpened={isOpened}>
+              <MainMenu style={{height: '50vh'}}/>
+          </Collapse>
+          <Logo />
+          {
+              window.screen.width <= 1000 && window.screen.height <= 1000
+                  ?  null
+                  : <MainMenu/>
+
+          }
+        {props.status ? <SearchInput /> : null}
         <Extras />
       </div>
     </header>
