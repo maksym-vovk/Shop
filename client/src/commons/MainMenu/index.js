aacdf84f934a0c439d198e42d292873a9f793c7a
Ivan Kravchenko <aivan.kr@gmail.com>
2019-10-03 10:00:35 +0300

    Header new (#91)
    * header fixed plus burger added
    
    * header-new fix
    
    * remove close-btn
    
    * close after redirect
    
    * style fix
    
    * style search mob
    
    * commit

--- client/src/commons/MainMenu/index.js
+++ client/src/commons/MainMenu/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import useWindowDimensions from '../Header';
 
 import './index.scss';
+import { SearchInput } from '../SearchInput';
 
 export const MainMenu = () => {
+  const {width} = useWindowDimensions()
   return (
     <nav className='main-menu'>
+      {width < 768 ? <SearchInput /> : null}
+
       <NavLink className='main-menu__item' to="/all">All</NavLink>
       <NavLink className='main-menu__item' to="/origin">Origin</NavLink>
       <NavLink className='main-menu__item' to="/nike">Nike</NavLink>
