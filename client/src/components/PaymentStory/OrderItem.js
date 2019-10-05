843cec6e4a5f60a95a7a982e07ffaa4d98706e9c
Nikita <ultranikit@gmail.com>
2019-10-05 14:05:59 +0300

    Payment story (#98)
    * payment-user-story layout
    
    * eslint fix
    
    * transition for collapse-button, and order date
    
    * formated  order-date
    
    * merge dev

---
+++ client/src/components/PaymentStory/OrderItem.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import {ProductList} from './ProductList';
+import './index.scss';
+
+const collapse = (event) => {
+  const collapseBtn = event.currentTarget;
+  const orderContent = collapseBtn.parentNode.querySelector('.order-item__content');
+
+  collapseBtn.classList.toggle('order-item--active');
+  if (orderContent.style.maxHeight) {
+    orderContent.style.maxHeight = null;
+  } else {
+    orderContent.style.maxHeight = orderContent.scrollHeight + 'px';
+  }
+};
+
+const dateFormat = (date) => {
+  const newDate = new Date(date);
+  let day = newDate.getDay();
+  switch (day) {
+    case 1 && 31:
+      day += 'st';
+      break;
+    case 2:
+      day += 'nd';
+      break;
+    case 3:
+      day += 'rd';
+      break;
+
+    default:
+      day += 'th'
+  }
+  const year = newDate.getFullYear();
+  const month = newDate.toLocaleString('default', { month: 'long' });
+  return String(`${month} ${day}, ${year}`);
+};
+
+export const OrderItem = ({cart, orderDate, orderStatus}) => {
+  return (
+    <React.Fragment>
+      <div className="order-item">
+        <button className="order-item__collapsible" onClick={(e) => collapse(e)}>
+          <span className="order-item__order-number">Order</span>
+          <span className="order-item__order-date">Date: { dateFormat(orderDate) }</span>
+        </button>
+        <div className="order-item__content">
+          <div className="payment-story-product-list__item-wrapper">
+
+            <ProductList cartItem={cart}/>
+            <div className="payment-story-product-list-item-status">
+              <p className="">
+                Status: {
+                  orderStatus
+                    ? <span className="payment-story-product-list-item-status__success">Success</span>
+                    : <span className="payment-story-product-list-item-status__fail">Not delivered</span>
+                }
+              </p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </React.Fragment>
+
+  )
+};
---
+++ client/src/components/PaymentStory/OrderList.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+
+import {connect} from 'react-redux';
+import {setUserOrders} from '../../store';
+import {EmptyPage} from '../EmptyPage'
+import {OrderItem} from './OrderItem';
+
+const mapStateToProps = state => ({
+  userOrders: state.user.userOrders,
+  user_id: state.user.userData._id
+});
+
+export const OrderList = connect(mapStateToProps, {setUserOrders})(props => {
+  const {user_id, userOrders, setUserOrders} = props;
+  /* eslint-disable */
+  useEffect(() => {
+    setUserOrders(user_id);
+  }, []);
+  /* eslint-enable */
+  const renderOrderItems = array => {
+    return array.map(order =>
+      <OrderItem cart={order.cart} orderDate={order.orderDate} orderStatus={order.orderStatus} key={order._id}/>
+    )
+  };
+
+  return (
+    userOrders
+      ? renderOrderItems(userOrders)
+      : <EmptyPage text="Order history is empty"/>
+  )
+});
