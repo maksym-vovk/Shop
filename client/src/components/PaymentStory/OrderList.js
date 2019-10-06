a9041576a879a41219f858bfab55ad6ca0bd3346
ultrahokage <ultranikit@gmail.com>
2019-10-06 18:29:32 +0300

    header background, order history some style change

--- client/src/components/PaymentStory/OrderList.js
+++ client/src/components/PaymentStory/OrderList.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import {connect} from 'react-redux';
 import {setUserOrders} from '../../store';
 import {EmptyPage} from '../EmptyPage'
 import {OrderItem} from './OrderItem';
+import { Lines } from 'react-preloaders';
 
 const mapStateToProps = state => ({
   userOrders: state.user.userOrders,
@@ -11,10 +12,12 @@ const mapStateToProps = state => ({
 });
 
 export const OrderList = connect(mapStateToProps, {setUserOrders})(props => {
+  const [loading, setLoading] = useState(true);
   const {user_id, userOrders, setUserOrders} = props;
   /* eslint-disable */
   useEffect(() => {
     setUserOrders(user_id);
+    setLoading(false)
   }, []);
   /* eslint-enable */
   const renderOrderItems = array => {
@@ -24,8 +27,12 @@ export const OrderList = connect(mapStateToProps, {setUserOrders})(props => {
   };
 
   return (
-    userOrders
-      ? renderOrderItems(userOrders)
-      : <EmptyPage text="Order history is empty"/>
+    <React.Fragment>
+      {userOrders
+        ? renderOrderItems(userOrders)
+        : <EmptyPage text="Order history is empty"/>
+      }
+      <Lines customLoading={loading} time={300}/>
+    </React.Fragment>
   )
 });
